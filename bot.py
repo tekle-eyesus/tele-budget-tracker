12b62158f6553424a9208056ec7ab4da465f3692
tekle-eyesus <tekleeyesus21@gmail.com>
2025-12-05 05:09:12 -0700

    add the main driver

--- bot.py
+++ bot.py
@@ -1,1 +1,34 @@
-print("GATE......")
+import asyncio
+import logging
+import os
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+from data.database import init_db
+from handlers import common, expenses, statistics
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def main():
+    # 1. Init Database
+    await init_db()
+
+    # 2. Setup Bot & Dispatcher
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+
+    # 3. Register Routers
+    dp.include_router(common.router)
+    dp.include_router(expenses.router)
+    dp.include_router(statistics.router)
+
+    # 4. Start Polling
+    print("Bot is running...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Bot stopped")
